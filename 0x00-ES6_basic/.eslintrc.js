464d21bb3876edab0dadbcff2e1432c8695e49cb
Shimaa Salah <shimaasalah91.13@gmail.com>
2024-05-27 22:49:24 +0300

    es6

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,18 +1,17 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
-      jest: true,
-    },
-    extends: [
-      'airbnb-base',
-    ],
-    parserOptions: {
-      ecmaVersion: 12,
-      sourceType: 'module',
-    },
-    rules: {
-      // Add custom rules here
-    },
-  };
-  
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    // Add custom rules here if needed
+  },
+};
