66cc06a441544ee1aef7ea542ebe1459cefb35d3
Shimaa Salah <shimaasalah91.13@gmail.com>
2024-06-01 20:02:23 +0300

    es6

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({ status: 200, body: 'Success' }))
-    .catch(() => Error)
+    .catch(() => Error())
     .finally(() => console.log('a response from the API'));
 }
